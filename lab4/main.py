6ab87e86f01778a2e6eea01562a975738bd002d2
nguyenkhai <khainq.bi10-085@st.usth.edu.vn>
2021-04-28 09:53:23 +0700

    last

--- lab4/main.py
+++ lab4/main.py
@@ -7,4 +7,18 @@ from domain.Mark import *
 
 op = curses.initscr() 
 curses.start_color()
+class Main:
+    a=Main.number_of_student()
+    for i in range(a):
+        Main.students_information()
+    Main.students_list()
+    #----
+    b=Main.number_of_course()
+    for i in range(b):
+        Main.course_information()
+    Main.courses_list()
+    #----
+    Main.mark_information()
+    Main.mark_list()
 
+    Main.gpa()
